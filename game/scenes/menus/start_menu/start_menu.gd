4195d4862cfc88beae700e18c101ec7495730a52
Noah Bumgardner <noah@playgamecafe.com>
2025-12-13 10:38:19 -0600

    Add Navigation to Settings and Credits Scenes

--- game/scenes/menus/start_menu/start_menu.gd
+++ game/scenes/menus/start_menu/start_menu.gd
@@ -7,8 +7,14 @@ func _ready() -> void:
 		buttonQuit.visible = false
 
 #region button pressed
+func _on_button_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/credits_menu/credits_menu.tscn")
+
 func _on_button_quit_pressed():
 	get_tree().quit()
+
+func _on_button_settings_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu/settings_menu.tscn")
 
 func _on_button_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/level1/level1.tscn")

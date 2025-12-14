1c72d81dc9258ef75fa259dbd55c0e0317a90c3b
Noah Bumgardner <noah@playgamecafe.com>
2025-12-13 19:06:29 -0600

    Define Signals to Trigger Remaining SFX

--- game/globals/event_bus.gd
+++ game/globals/event_bus.gd
@@ -1,7 +1,54 @@
 extends Node
 
+#region Enemy signals
 @warning_ignore("unused_signal")
-signal globalUiElementMouseEntered();
+signal globalEnemyDestroyed();
 
 @warning_ignore("unused_signal")
+signal globalEnemyHurt();
+
+@warning_ignore("unused_signal")
+signal globalEnemyHoverEnd();
+
+@warning_ignore("unused_signal")
+signal globalEnemyHoverStart();
+#endregion Enemy signals
+
+#region Environment signals
+@warning_ignore("unused_signal")
+signal globalEnvironmentRiftAreaClosed();
+
+@warning_ignore("unused_signal")
+signal globalEnvironmentRiftAreaEntered();
+
+@warning_ignore("unused_signal")
+signal globalEnvironmentRiftAreaLeaveEarly();
+
+@warning_ignore("unused_signal")
+signal globalEnvironmentRiftBigEruption();
+
+@warning_ignore("unused_signal")
+signal globalEnvironmentRiftBigReadyToLaunch();
+#endregion Environment signals
+
+#region Player signals
+@warning_ignore("unused_signal")
+signal globalPlayerHurt();
+
+@warning_ignore("unused_signal")
+signal globalPlayerPowerUpGained();
+
+@warning_ignore("unused_signal")
+signal globalPlayerWalkEnd();
+
+@warning_ignore("unused_signal")
+signal globalPlayerWalkStart();
+#endregion Player signals
+
+#region UI signals
+@warning_ignore("unused_signal")
 signal globalUiElementSelected();
+
+@warning_ignore("unused_signal")
+signal globalUiElementMouseEntered();
+#endregion UI signals

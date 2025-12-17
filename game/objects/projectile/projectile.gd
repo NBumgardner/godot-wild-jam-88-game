470f65abca72503695fe132b170971b466829b80
Apples <2352020+apples@users.noreply.github.com>
2025-12-16 18:43:05 -0600

    Rare upgrades

--- game/objects/projectile/projectile.gd
+++ game/objects/projectile/projectile.gd
@@ -1,19 +1,42 @@
 extends Area2D
 class_name Projectile
 
+const HOMING_SPEED = TAU / 10.0
+
 @export var velocity: Vector2
 
+var bounces: int = 0
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var homing: Area2D = $Homing
+@onready var homing_shape: CollisionShape2D = $Homing/HomingShape
+
+func _ready() -> void:
+	homing_shape.disabled = GameState.player_stats.projectile_homing_radius == 0.0
+	homing_shape.shape.radius = GameState.player_stats.projectile_homing_radius
+	
 
 func _physics_process(delta: float) -> void:
+	if homing.has_overlapping_bodies():
+		var target := homing.get_overlapping_bodies()[0].global_position
+		var angle := velocity.angle_to(target - global_position)
+		velocity = velocity.rotated(sign(angle) * min(GameState.player_stats.projectile_homing_speed_mult * HOMING_SPEED * delta, abs(angle)))
+	
 	global_position += velocity * delta
-	sprite_2d.rotation = velocity.angle()
+	sprite_2d.frame_coords.x = floori((TAU / 4.0 - velocity.angle()) / TAU * 8.0)
+	
+	if not homing_shape.disabled:
+		homing_shape.position = velocity.normalized() * homing_shape.shape.radius
 
 
 func _on_body_entered(body: Node2D) -> void:
 	assert(body is Enemy)
 	body.hit()
-	queue_free()
+	if bounces < GameState.player_stats.projectile_bounce:
+		bounces += 1
+		velocity = velocity.rotated(randf() * TAU)
+	else:
+		queue_free()
 
 
 func _on_life_timer_timeout() -> void:
--- game/scenes/intermission/reward_choice.gd
+++ game/scenes/intermission/reward_choice.gd
@@ -3,20 +3,32 @@ extends Sprite2D
 @export var upgrade: PlayerStats.Upgrade:
 	set(v):
 		upgrade = v
-		if sprite_2d:
-			sprite_2d.texture = PlayerStats.get_upgrade_icon(upgrade)
-		if label:
-			label.text = PlayerStats.Upgrade.find_key(upgrade)
+		_update()
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var label: Label = $Label
+@onready var rare_shine: Polygon2D = $RareShine
 
 var _time: float = 0.0
 
 func _ready() -> void:
-	sprite_2d.texture = PlayerStats.get_upgrade_icon(upgrade)
-	label.text = PlayerStats.Upgrade.find_key(upgrade)
+	_update()
 
 func _process(delta: float) -> void:
 	_time += delta
 	rotation = sin(_time) * 0.3
+	rare_shine.rotation += delta
+	rare_shine.scale = Vector2.ONE * abs(sin(_time))
+
+func _update() -> void:
+	if not is_inside_tree():
+		return
+	sprite_2d.texture = PlayerStats.get_upgrade_icon(upgrade)
+	label.text = PlayerStats.Upgrade.find_key(upgrade)
+	if PlayerStats.is_upgrade_rare(upgrade):
+		rare_shine.visible = true
+		self_modulate = Color(1.0, 0.75, 0.0, 1.0)
+		rare_shine.self_modulate = self_modulate
+	else:
+		rare_shine.visible = false
+		self_modulate = Color.WHITE

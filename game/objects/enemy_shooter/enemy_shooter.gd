1b68bf16a05ced7c2e3134f80f3b7eb807f85098
RPDak <x-manlozano@live.com>
2025-12-20 21:29:49 -0600

    Added shooter enemy sprites/animations

--- game/objects/enemy_shooter/enemy_shooter.gd
+++ game/objects/enemy_shooter/enemy_shooter.gd
@@ -8,6 +8,7 @@ const ACCURACY_RADIUS = 96.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var bt_player: BTPlayer = $BTPlayer
+@onready var anim_tree: AnimationTree = $AnimationTree
 
 func _ready() -> void:
 	bt_player.blackboard.set_var("guard_zone", guard_zone)
@@ -18,6 +19,7 @@ func _ready() -> void:
 		guard_zone.player_aggrod.connect(_on_player_aggrod)
 
 func _physics_process(_delta: float) -> void:
+	anim_tree.set("parameters/blend_position",remap(velocity.x, SPEED,-SPEED,1,-1))
 	if velocity.x > 0:
 		sprite_2d.flip_h = true
 	elif velocity.x < 0:

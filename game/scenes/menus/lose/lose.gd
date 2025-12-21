1be0d3bceef0adec879164b85486ddd8e3dbd816
Apples <2352020+apples@users.noreply.github.com>
2025-12-21 14:25:55 -0600

    Game over music

--- game/scenes/menus/lose/lose.gd
+++ game/scenes/menus/lose/lose.gd
@@ -1,8 +1,5 @@
 extends Control
 
-func _ready() -> void:
-	EventBus.globalCreditsEntered.emit()
-
 func _on_button_back_pressed():
 	EventBus.globalUiElementSelected.emit()
 	get_tree().change_scene_to_file("res://scenes/menus/credits_menu/credits_menu.tscn")
